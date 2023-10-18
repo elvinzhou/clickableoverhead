e6557e504443805f498e2d933e5d7644ba807bb7
Elvin Zhou <elvinzhou@gmail.com>
2023-10-18 03:12:09 -0500

    update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
 const nextConfig = {
-    webpack:(config) => {
+    webpack:(config,{isServer}) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()]
+          }
         config.module.rules.push({
             test: /\.svg$/,
             use: [{loader:'@svgr/webpack',options:{svgoCOnfig:'./svgo.config.js'}}],
